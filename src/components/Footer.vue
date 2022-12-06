5f29773862bfb159d03f77916ac0779811581603
Vladimir <justlikevova@gmail.com>
2022-12-06 21:07:54 +0300

    footer refactor

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="footer">
-    <footer>
-      2022 Capture Helper. Разработчик: <a href="https://t.me/Squishy666">@Squishy666</a>
+    <footer class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <p class="text-center text-muted">© 2022 Capture Helper</p>
+      <p class="text-center text-muted"> Разработчик: <a href="https://t.me/Squishy666">@Squishy666</a></p>
     </footer>
   </div>
 </template>
