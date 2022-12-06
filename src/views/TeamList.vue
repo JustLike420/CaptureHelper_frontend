6e710cd0b8963724f245244574a604efd826382a
Vladimir <justlikevova@gmail.com>
2022-12-07 00:30:41 +0300

    team list
    refactoring
    dynamic pagination

--- src/views/TeamList.vue
+++ src/views/TeamList.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <div v-for="team in teamList" :key="team.id" class="card mb-3" style="max-width: 540px;">
-      <div class="row g-0">
-        <div class="col-md-8">
-          <div class="card-body">
-            <a href="#" @click="goTo(team.id)"><h5 class="card-title">{{ team.name }}</h5></a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <img :src="team.logo" class="img-fluid rounded-start logo" alt="...">
-        </div>
-      </div>
+  <div class="list-group w-auto align-items-center">
+    <div v-for="(team, index) in teamList" :key="team.id" class="text-center list-group-item col-md-6"
+         style="">
+      <a @click="goTo(team.id)" class="row" style="text-decoration: none;color:black;">
+        <h5 class="col-md-1 align-self-center">{{ index + 1 }}</h5>
+        <h5 class="col-md-3 align-self-center">{{ team.name }}</h5>
+        <div class="col-md-6"></div>
+        <img :src="team.logo" class="col-md-6 rounded-circle" style="height: 90px; width: 90px">
+      </a>
     </div>
-    </div>
+    <div ref="observer" class="observer"></div>
+  </div>
 </template>
 
 <script>
@@ -20,21 +18,56 @@ export default {
   name: "TeamList",
   data() {
     return {
-      teamList: []
+      teamList: [],
+      page: 2,
+      total: 0
     }
   },
   created() {
-    this.loadListTeams()
+    this.loadListTeams(this.page)
   },
   methods: {
     async loadListTeams() {
       this.teamList = await fetch(
-          `${this.$store.getters.getServerUrl}/team`
+          `${this.$store.getters.getServerUrl}/team?page=1`
       ).then(response => response.json())
+          .then(response => {
+            return response.results
+          })
     },
     goTo(id) {
-      this.$router.push({ name: 'TeamSingle', params: {id:id} })
+      this.$router.push({name: 'TeamSingle', params: {id: id}})
+    },
+    async loadMoreTeams(pageNumber) {
+      await fetch(
+          `${this.$store.getters.getServerUrl}/team?page=${pageNumber}`
+      ).then((response) => {
+        if (response.status === 404) {
+          throw new Error("Bad response from server");
+        }
+        return response;
+      }).then((returnedResponse) => returnedResponse.json())
+          .then(response => {
+                this.page += 1
+                this.teamList = [...this.teamList, ...response.results]
+                return response.results
+              }
+          )
     }
+  },
+
+  mounted() {
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        this.loadMoreTeams(this.page)
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer)
   }
 }
 </script>
